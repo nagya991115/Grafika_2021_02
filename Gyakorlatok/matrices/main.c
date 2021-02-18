14f3d151c777a6fb258435d71592d654c0a6529c
nagya991115 <78906736+nagya991115@users.noreply.github.com>
2021-02-18 13:59:28 +0100

    Add files via upload

---
+++ Gyakorlatok/matrices/main.c
@@ -0,0 +1,33 @@
+#include "matrix.h"
+
+int main(int argc, char* argv[])
+{
+	float a[3][3] = {
+        { 1.0f, -2.0f,  3.0f},
+        { 5.0f, -3.0f,  0.0f},
+        {-2.0f,  1.0f, -4.0f}
+    };
+	float b[3][3];
+	float c[3][3];
+	float d[3][3];
+    init_zero_matrix(b);
+    b[1][1] =  8.0f;
+    b[2][0] = -3.0f;
+    b[2][2] =  5.0f;
+
+    print_matrix(a);
+    print_matrix(b);
+    
+    add_matrices(a, b, c);
+
+    print_matrix(c);
+	init_identity_matrix(d);
+	print_matrix(d);
+	
+	float result[3][3];
+	mult_By_Scalar(result,2.0,d);
+	print_matrix(result);
+
+	return 0;
+}
+
---
+++ Gyakorlatok/matrices/matrix.h
@@ -0,0 +1,24 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+/**
+ * Initializes all elements of the matrix to zero.
+ */
+void init_zero_matrix(float matrix[3][3]);
+
+/**
+ * Print the elements of the matrix.
+ */
+void print_matrix(const float matrix[3][3]);
+
+/**
+ * Add matrices.
+ */
+void add_matrices(const float a[3][3], const float b[3][3], float c[3][3]);
+
+
+void init_identity_matrix(float a[3][3]);
+
+void mult_By_Scalar(float result[3][3], float scalar,const float a[3][3]);
+#endif // MATRIX_H
+
